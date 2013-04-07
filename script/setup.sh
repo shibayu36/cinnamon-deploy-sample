1148273f873a8bf7e341b6d5dc1c8de0faa09ed0
shiba_yu36 <shibayu36@gmail.com>
2013-04-07 14:08:22 +0900

    デプロイ用セットアップ

---
+++ script/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo aptitude install -y build-essential
+sudo aptitude install -y curl
+sudo aptitude install -y git-core git-doc
+sudo aptitude install -y daemontools daemontools-run
+curl -L http://cpanmin.us | perl - --sudo App::cpanminus
